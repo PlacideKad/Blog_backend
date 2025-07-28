9461b9c4191f282ad9d1ea7949e55d7722bec2e3
PlacideKad <placidekad@gmail.com>
2025-07-28 18:15:50 +0200

    update the response to the client in case he is authenticated or not

--- routes/user.js
+++ routes/user.js
@@ -4,14 +4,14 @@ import jwt from 'jsonwebtoken';
 const router=Router();
 router.get('/api/user',(req,res)=>{
   const token=req.cookies.auth_token;
-  if(!token) return res.status(401).send({message:'Not authenticated'});
+  if(!token) return res.status(401).send({message:'Not authenticated',authenticated:false});
   try{
     const decoded=jwt.verify(token,process.env.JWT_SECRET);
     if(!decoded) throw new Error('Cookie expired');
-    res.status(200).send({message:"Authenticated successfully!"})
+    res.status(200).send({message:"Authenticated successfully!",authenticated:true})
   }catch(err){
     console.error(err);
-    res.status(401).send({message:'Invalid cookie or cookie expired. Please try to reconnect'})
+    res.status(401).send({message:'Invalid cookie or cookie expired. Please try to reconnect',authenticated:false});
   }
 
 })
