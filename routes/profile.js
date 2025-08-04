85a3b2489307645d494805353e777bab0ffc900f
PlacideKad <placidekad@gmail.com>
2025-08-04 17:08:28 +0200

    Added a new route to send the user profile picture, avoiding the google blocade, as we cannot use img src to updated the user profile picture when he is logged in

--- routes/profile.js
+++ routes/profile.js
@@ -5,7 +5,7 @@ const router=Router();
 router.get('/api/profile',(req,res)=>{
   if(!req.isAuthenticated()) return res.redirect('/api/redirect');
   const user=req.user;
-  const token=jwt.sign({id:user.id},process.env.JWT_SECRET,{expiresIn:'1h'});
+  const token=jwt.sign({id:user.id,name:user.name,email:user.email,picture:user.picture},process.env.JWT_SECRET,{expiresIn:'1h'});
   res.cookie('auth_token',token,{
     httpOnly:true,
     secure:false,//true en prod
@@ -15,10 +15,10 @@ router.get('/api/profile',(req,res)=>{
   res.redirect('http://localhost:5173');
 });
 
-router.get('api/logout',(req,res)=>{
+router.get('/api/logout',(req,res)=>{
   req.logout(()=>{
     res.clearCookie('auth_token');
-    res.redirect('/api/redirect');
+    res.send({disconnected:true});
   });
 })
 export default router
--- routes/user.js
+++ routes/user.js
@@ -8,11 +8,17 @@ router.get('/api/user',(req,res)=>{
   try{
     const decoded=jwt.verify(token,process.env.JWT_SECRET);
     if(!decoded) throw new Error('Cookie expired');
-    res.status(200).send({message:"Authenticated successfully!",authenticated:true})
+    res.status(200).send({message:"Authenticated successfully!",authenticated:true,decoded})
   }catch(err){
     console.error(err);
     res.status(401).send({message:'Invalid cookie or cookie expired. Please try to reconnect',authenticated:false});
   }
-
+});
+router.get('/api/user/avatar',async (req,res)=>{
+  const url=req.query.url;
+  const response=await fetch(url);
+  const buffer=await response.arrayBuffer();
+  res.setHeader('Content-Type','image/jpeg');
+  res.send(Buffer.from(buffer));
 })
 export default router;
