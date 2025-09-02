a641b7edf4e206bc2b22e1199bafac3939050925
PlacideKad <placidekad@gmail.com>
2025-09-02 04:18:30 +0200

    updated the stash model

--- model/stash.js
+++ model/stash.js
@@ -25,12 +25,15 @@ const stashSchema=new Schema({
   },
   from_article:{
     type:Schema.Types.ObjectId,
-    ref:'article'
+    ref:'article' 
   },
   cover:{
     type:linkSchema,
     default:{link:"https://images.unsplash.com/photo-1455390582262-044cdead277a?q=80&w=1973&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}
   },
+  related_files:{
+    type:[linkSchema],
+  }
 },{strict:true,timestamps:true});
 
 export const Stash=model('stash',stashSchema);
