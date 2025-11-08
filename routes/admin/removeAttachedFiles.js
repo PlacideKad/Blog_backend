30ddc5901f444ff74e22dd3826648d35735d5af0
Placide <placidekad@gmail.com>
2025-11-08 10:36:59 +0200

    added admin authentication middleware to all admin routes

--- routes/admin/removeAttachedFiles.js
+++ routes/admin/removeAttachedFiles.js
@@ -3,6 +3,7 @@ import { Types } from "mongoose";
 import { Stash } from "../../model/stash.js";
 import { Article } from "../../model/article.js";
 import { deleteAttachedFile } from "../../middleware/deleteUselessData.js";
+import checkUserIsAdmin from "../../middleware/checkUserIsAdmin.js";
 
 const router=Router();
 //dans ce middleware on supprime le fichier indesire de cloudinary.
@@ -14,21 +15,24 @@ const router=Router();
 // si l'appel est fait depuis edit stash from_stash doit etre true
 //si l'appel est fait depuis edit article from_stash doit etre false
 //on retourne le document stash pour mettre a jour notre interface
-router.patch('/api/admin/delete/cloudinary/file/:id',deleteAttachedFile,async (req,res)=>{
-    const id=new Types.ObjectId(`${req.params.id}`);
-    const {from_stash}=req.body;
-    const {related_files}=req;
-    try{
-        const update=from_stash? 
-        await Stash.findByIdAndUpdate(id,{$set:{related_files:related_files}},{new:true}):
-        await Article.findByIdAndUpdate(id,{$set:{related_files:related_files}},{new:true});
+router.patch('/api/admin/delete/cloudinary/file/:id',
+	checkUserIsAdmin,
+  deleteAttachedFile,
+  async (req,res)=>{
+		const id=new Types.ObjectId(`${req.params.id}`);
+		const {from_stash}=req.body;
+		const {related_files}=req;
+		try{
+				const update=from_stash? 
+				await Stash.findByIdAndUpdate(id,{$set:{related_files:related_files}},{new:true}):
+				await Article.findByIdAndUpdate(id,{$set:{related_files:related_files}},{new:true});
 
-        if(!update) throw new Error(`Error while updating a ${from_stash?'stash':'article'} after removing one of its related files from cloudinary`)
-        res.status(200).send(from_stash?{success:true, stash:update}:{sucess:true, article:update});
-    }catch(err){
-        console.log(err);
-        res.status(500).send({sucess:false, message:err});
-    }
-})
+				if(!update) throw new Error(`Error while updating a ${from_stash?'stash':'article'} after removing one of its related files from cloudinary`)
+				res.status(200).send(from_stash?{success:true, stash:update}:{sucess:true, article:update});
+		}catch(err){
+				console.log(err);
+				res.status(500).send({sucess:false, message:err});
+	}
+});
 
 export default router;
--- routes/admin/stash.js
+++ routes/admin/stash.js
@@ -4,11 +4,14 @@ import { Types } from "mongoose";
 import { deleteCloudinaryCoversArray ,deleteRelatedFiles, deleteCloudinaryCover} from "../../middleware/deleteUselessData.js";
 import uniqueString from "unique-string";
 import { v2 as cloudinary} from "cloudinary";
+import checkUserIsAdmin from "../../middleware/checkUserIsAdmin.js";
 
 const router=Router();
+
 const getCloudinaryLinkFromPublicId=(public_id)=>{
   return `https://res.cloudinary.com/${process.env.CLOUDINARY_CLOUD_NAME}/image/upload/${public_id}`;
 }
+
 const copyCloudinaryItem=async (public_id)=>{
   try{
     const res=await cloudinary.uploader.upload(getCloudinaryLinkFromPublicId(public_id),
@@ -20,45 +23,50 @@ const copyCloudinaryItem=async (public_id)=>{
     return null;
   }
 }
-router.post('/api/admin/stash',deleteCloudinaryCoversArray,async (req,res)=>{
-  const {body}=req;
-  try{
-    if(body.stash_id){
-      const id=new Types.ObjectId(`${body.stash_id}`);
-      const updatedStash= await Stash.findByIdAndUpdate(id,{...body,stash_id:undefined},{runValidators:true});
-      if(!updatedStash) throw new Error('Error when updating a stashed work');
-      return res.status(200).send({success:true,stash_id:updatedStash._id});
-    }else{
-      const id=body.article_id?new Types.ObjectId(`${body.article_id}`):null;
-      //this is where we need to create a copy of ALL the related files and cover using random string at the end of the new display_name
-      //As the body already contains the related files and the cover, the data that'll be used to create the new Stash will take the copied values
-      let newRelatedFiles=[];
-      let newCoverLink=null;
-      if(id){
-        if(body.related_files && body.related_files.length>0){
-          for(const file of body.related_files){
-            const newDisplayName=await copyCloudinaryItem(file.display_name);
-            if(newDisplayName) newRelatedFiles.push({...file,display_name:newDisplayName,file:{link:getCloudinaryLinkFromPublicId(newDisplayName)}});
+
+router.post('/api/admin/stash',
+  checkUserIsAdmin,
+  deleteCloudinaryCoversArray,
+  async (req,res)=>{
+    const {body}=req;
+    try{
+      if(body.stash_id){
+        const id=new Types.ObjectId(`${body.stash_id}`);
+        const updatedStash= await Stash.findByIdAndUpdate(id,{...body,stash_id:undefined},{runValidators:true});
+        if(!updatedStash) throw new Error('Error when updating a stashed work');
+        return res.status(200).send({success:true,stash_id:updatedStash._id});
+      }else{
+        const id=body.article_id?new Types.ObjectId(`${body.article_id}`):null;
+        //this is where we need to create a copy of ALL the related files and cover using random string at the end of the new display_name
+        //As the body already contains the related files and the cover, the data that'll be used to create the new Stash will take the copied values
+        let newRelatedFiles=[];
+        let newCoverLink=null;
+        if(id){
+          if(body.related_files && body.related_files.length>0){
+            for(const file of body.related_files){
+              const newDisplayName=await copyCloudinaryItem(file.display_name);
+              if(newDisplayName) newRelatedFiles.push({...file,display_name:newDisplayName,file:{link:getCloudinaryLinkFromPublicId(newDisplayName)}});
+            }
+          }
+          if(body.cover && body.cover.link && body.cover.link!==process.env.DEFAULT_STASH_LINK & body.cover.link!==process.env.DEFAULT_PUBLISHED_LINK){
+            const publicId=body.cover.link.split('/').pop();
+            const newCoverPublicId=await copyCloudinaryItem(publicId);
+            newCoverLink=newCoverPublicId?getCloudinaryLinkFromPublicId(newCoverPublicId):null;
           }
         }
-        if(body.cover && body.cover.link && body.cover.link!==process.env.DEFAULT_STASH_LINK & body.cover.link!==process.env.DEFAULT_PUBLISHED_LINK){
-          const publicId=body.cover.link.split('/').pop();
-          const newCoverPublicId=await copyCloudinaryItem(publicId);
-          newCoverLink=newCoverPublicId?getCloudinaryLinkFromPublicId(newCoverPublicId):null;
-        }
+        const newStash=body.article_id?new Stash({...body,article_id:undefined,from_article:id, relatedFiles:newRelatedFiles,cover:{link:newCoverLink}}):new Stash(body);
+        const savedStash=await newStash.save();
+        if(!savedStash) throw new Error('Error occured when creating a new stash');
+        return res.status(201).send({success:true, stash_id:savedStash._id});
       }
-      const newStash=body.article_id?new Stash({...body,article_id:undefined,from_article:id, relatedFiles:newRelatedFiles,cover:{link:newCoverLink}}):new Stash(body);
-      const savedStash=await newStash.save();
-      if(!savedStash) throw new Error('Error occured when creating a new stash');
-      return res.status(201).send({success:true, stash_id:savedStash._id});
+    }catch(err){
+      console.log(err);
+      res.status(400).send({error:err,success:false});
     }
-  }catch(err){
-    console.log(err);
-    res.status(400).send({error:err,success:false});
   }
-});
+);
 
-router.get('/api/admin/stashes',async (req,res)=>{
+router.post('/api/admin/stashes',async (req,res)=>{
   const limit=parseInt(req.query.limit) || 6;
   const page=parseInt(req.query.page) || 1;
   const skip=(page-1)*limit;
