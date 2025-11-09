553a481021256eeb98689311151676050d22076a
Placide <placidekad@gmail.com>
2025-11-09 08:56:39 +0200

    ready for prod

--- routes/profile.js
+++ routes/profile.js
@@ -8,11 +8,11 @@ router.get('/api/profile',createUser,(req,res)=>{
   const token=jwt.sign({id:user.id},process.env.JWT_SECRET,{expiresIn:'1h'});
   res.cookie('auth_token',token,{
     httpOnly:true,
-    secure:false,//true en prod, false en dev
+    secure:true,//true en prod, false en dev
     sameSite:'lax',
     maxAge:1000*60*60
   });
-  res.redirect(process.env.FRONTEND_HOME_DEV);
+  res.redirect(process.env.FRONTEND_HOME_PROD);
 });
 
 router.get('/api/logout',(req,res)=>{
--- routes/redirect.js
+++ routes/redirect.js
@@ -3,7 +3,7 @@ import { Router } from "express";
 const router=Router();
 
 router.get('/api/redirect',(req,res)=>{
-  res.redirect(process.env.FRONTEND_HOME_DEV);
+  res.redirect(process.env.FRONTEND_HOME_PROD);
 });
 
 export default router;
