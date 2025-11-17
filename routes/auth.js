bbd4d7094feea282ddac5fa0e1abded8e1cac451
Placide <placidekad@gmail.com>
2025-11-17 18:37:29 +0200

    authentication successful

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { User } from "../model/user.js";
 import { getHashedPassword , checkPassword} from "../utils/hasher.js";
-import { disconnect } from "mongoose";
+import { Types } from "mongoose";
 
 const router= Router();
 
@@ -21,7 +21,11 @@ router.post('/api/authenticate/signup',async (req,res)=>{
     });
     const savedUser=await newUser.save();
     if(!savedUser) return res.status(500).send({success:false,errorHandled:true,message:'Unexpected error. User not created'});
-    return res.send({success:true,user:savedUser});
+    req.session.userId=savedUser._id;
+    res.session.save(err=>{
+      if(err) throw new Error("Not signed in");
+      return res.send({success:true,user:savedUser});
+    })
   }catch(err){
     console.log(err);
     return res.status(500).send({success:false, errorHandled:false, message:err});
@@ -34,12 +38,24 @@ router.post("/api/authenticate/signin",async (req,res)=>{
     if(!(email && password)) return res.status(400).send({success:false, errorHandled:true, message:"Missing email or password. Check yout input, then submit again"});
     const foundUser=await User.findOne({email:email},{},{lean:true});
     if(!foundUser) return res.status(400).send({success:false, errorHandled:true, message:`No user with the email ${email} found. Please, sign up first.`});
-    if(!checkPassword(password,foundUser.password)) return res.status(401).send({success:false, errorHandled:true, message:"Email or password might be incorrect. Please check your entries"});
-    return res.status(200).send({success:true, user:foundUser});
+    if(!await checkPassword(password,foundUser.password)) return res.status(401).send({success:false, errorHandled:true, message:"Email or password might be incorrect. Please check your entries"});
+    req.session.userId=foundUser._id;
+    req.session.save(err=>{
+      if(err) throw new Error("Not logged In");
+      return res.status(200).send({success:true, user:foundUser});
+    })
+    
   }catch(err){
     console.log(err);
     return res.status(500).send({success:false, errorHandled:false, message:"Unexped error occured"});
   }
+})
+
+router.get("/api/authenticate/check_user",async (req,res)=>{
+  if(!req.session.userId) return res.send({user:null, success:false});
+  const id=new Types.ObjectId(`${req.session.userId}`);
+  const foundUser=await User.findById(id,{},{lean:true});
+  return res.send({success:true,user:foundUser});
 })
 
 router.get('/api/logout',(req,res)=>{
