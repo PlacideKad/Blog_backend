cf3de6d169bfadaab9dffb5646b76b11f4f892d2
Placide <placidekad@gmail.com>
2025-11-20 04:16:41 +0200

    fixed a bug with deleting default profile picture

--- routes/cloudinaryRequests.js
+++ routes/cloudinaryRequests.js
@@ -4,15 +4,17 @@ const router=Router();
 // this is the route used to remove client profile picture
 router.post('/api/remove_cloudinary',(req,res)=>{
   const publicId=req.body.publicId;
-  try{
-    cloudinary.uploader.destroy(publicId,(error,result)=>{
-      if(error) throw new Error('Error deleting item from cloudinary');
-      return res.status(200).send({success:true,message:'Item deleted successfully from cloudinary',result});
-    });
-
-  }catch(err){
-    console.log(err);
-    return res.status(500).send({success:false,message:'Error deleting item from cloudinary'});
+  if(publicId!==process.env.DEFAULT_PROFILE_PICTURE_DISPLAY_NAME){
+    try{
+      cloudinary.uploader.destroy(publicId,(error,result)=>{
+        if(error) throw new Error('Error deleting item from cloudinary');
+        return res.status(200).send({success:true,message:'Item deleted successfully from cloudinary',result});
+      });
+  
+    }catch(err){
+      console.log(err);
+      return res.status(500).send({success:false,message:'Error deleting item from cloudinary'});
+    }
   }
 });
 
